893c98dcbeaa6580277498778d0d0f78e2f3b53c
nmnj222 <nemanjavlaovic@protonmail.com>
2024-04-14 22:59:12 +0200

    landing page implemented

--- stedisa/app/Config/Autoload.php
+++ stedisa/app/Config/Autoload.php
@@ -90,5 +90,5 @@ class Autoload extends AutoloadConfig
      *
      * @var list<string>
      */
-    public $helpers = [];
+    public $helpers = ['http://localhost:8080/htdocs/app/stedisa/stedisa'];
 }
